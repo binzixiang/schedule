f3456081b9a56d2ec27da1e5ef86977b2a1265d7
YiQiu <binzixiang@163.com>
2024-10-30 13:21:31 +0800

    fix: pages: replace the interface

--- lib/pages/function/pages/function_exam_plan/logic.dart
+++ lib/pages/function/pages/function_exam_plan/logic.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:schedule/common/api/schedule/schedule_query_api.dart';
+import 'package:schedule/common/api/schedule/v2/schedule_query_api_v2.dart';
 import 'package:schedule/global_logic.dart';
 
 import 'state.dart';
@@ -7,7 +7,7 @@ import 'state.dart';
 class FunctionExamPlanLogic extends GetxController {
   final FunctionExamPlanState state = FunctionExamPlanState();
   final globalState = Get.find<GlobalLogic>().state;
-  final queryApi = ScheduleQueryApi();
+  final queryApi = ScheduleQueryApiV2();
 
 
   void init() {
